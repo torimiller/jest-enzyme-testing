8e06229bdccec524fcd10ca6a0dc4736108e2920
Tori Miller <millertori04@gmail.com>
2022-01-08 16:18:55 -0800

    added styling

--- src/App.js
+++ src/App.js
@@ -1,19 +1,10 @@
+import Header from './component/header';
+import './app.scss';
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Header />
     </div>
   );
 }
